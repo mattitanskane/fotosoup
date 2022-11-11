b516539b57d69e793af37beee7e7aeb51850ca09
mattitanskane <mattitapanitanskanen@gmail.com>
2022-11-11 23:16:31 +0200

    Add simple configuration management

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from PIL import Image
 import datetime
 import regex
 import typer
-
+import configparser
+from pathlib import Path
 
 app = typer.Typer()
 
@@ -80,8 +81,63 @@ def finalConfirmation(filename):
 
     return final_answers
 
+def config():
+    config_file = Path("./fotosoup.ini")
+
+    if config_file.is_file():
+        print('Using existing config file.')
+
+        config = configparser.ConfigParser()
+        config.read('fotosoup.ini')
+
+        return True
+
+    def config_file_exists(answers):
+        # PyInquirer seems to need a function and a boolean is not enough
+        return config_file.is_file()
+
+    default_input = './input/'
+    default_output = './output/'
+
+    config_questions = [
+            {
+                'type': 'confirm',
+                'name': 'config_missing',
+                'message': f'Config file missing. Use default config?',
+                'when': not config_file_exists
+            },
+            {
+                'type': 'input',
+                'name': 'input_directory',
+                'message': 'Path for input directory:',
+                'default': default_input,
+                'when': lambda answers: not answers['config_missing'],
+            },
+            {
+                'type': 'input',
+                'name': 'output_directory',
+                'message': 'Path for output directory:',
+                'default': default_output,
+                'when': lambda answers: not answers['config_missing'],
+            }
+        ]
+    config_answers = prompt(config_questions)
+
+    input_directory = config_answers['input_directory'] if not config_answers['config_missing'] else default_input
+    output_directory = config_answers['output_directory'] if not config_answers['config_missing'] else default_output
+
+    config = configparser.ConfigParser()
+    config['DEFAULT'] = {'InputDirectory': input_directory,
+                        'OutputDirectory': output_directory}
+    with open('fotosoup.ini', 'w') as configfile:
+        config.write(configfile)
+
+    return True
+
 @app.command()
 def format(path: str, single: bool = False):
+    config()
+
     image = Image.open(path)
     exif = image.getexif()
     creation_time = exif.get(306)
